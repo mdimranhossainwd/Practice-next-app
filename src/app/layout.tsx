e5d5c6908ab7056e648a6ed96bf28f87863d1696
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-04-25 07:32:26 +0600

    feat(footer): Added to footer sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/shared/Footer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -29,7 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <div className="container mx-auto">{children}</div>
+        <Footer />
       </body>
     </html>
   );
