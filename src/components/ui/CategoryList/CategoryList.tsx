872440f4737d476f90e15cc2a3673a93359dcc68
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-04-28 16:27:34 +0600

    feat(categories): Categories menu layout added

--- src/components/ui/CategoryList/CategoryList.tsx
+++ src/components/ui/CategoryList/CategoryList.tsx
@@ -1,7 +1,31 @@
 import { getAllCategory } from "@/utils/getAllCategory";
-
+import { Box, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 export default async function CategoryList() {
-  const data = await getAllCategory();
-  console.log(data);
-  return <h2>Category List</h2>;
+  const { data: allCategory } = await getAllCategory();
+  // console.log(data);
+  return (
+    <Box className="text-black">
+      <Typography variant="h5" marginBottom={2} fontWeight={700} gutterBottom>
+        Categories
+      </Typography>
+
+      {allCategory?.map((category) => (
+        <Box
+          key={category._id}
+          sx={{
+            padding: 1,
+            margin: 1,
+            textAlign: "center",
+            border: "1px solid #ccc",
+            borderRadius: 2,
+          }}
+        >
+          <Link href={`/category/${category.name}`}>
+            <Stack spacing={2}>{category?.name}</Stack>
+          </Link>
+        </Box>
+      ))}
+    </Box>
+  );
 }
