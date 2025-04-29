948c5eebc4215983f6fe30f0e68cbdbfa0bfec51
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-04-29 09:51:53 +0600

    refactor(card): Fixed to card images height

--- src/app/catogories/[categoriesId]/page.tsx
+++ src/app/catogories/[categoriesId]/page.tsx
@@ -1,6 +1,13 @@
 import getAllNews from "@/utils/getAllNews";
-import { Card, CardContent, Container, Grid, Typography } from "@mui/material";
-import Image from "next/image";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 export default async function DynamicNews({ params, searchParams }) {
   const { data: allNewsData } = await getAllNews(searchParams?.category);
@@ -9,31 +16,39 @@ export default async function DynamicNews({ params, searchParams }) {
       <Grid container spacing={2}>
         {allNewsData?.map((news) => (
           <Grid item xs={12} sm={6} md={4} key={news._id}>
-            <Card sx={{ maxWidth: 345, mx: "auto" }} className="my-4">
-              <Image
-                src={news?.image}
-                width={1000}
-                height={600}
-                alt="Banner Logo"
-              />
-              <CardContent>
-                <span className="bg-red-500 w-[250px] px-4 py-1 mb-3 text-white rounded-sm">
-                  {news?.category}
-                </span>
-                <Typography gutterBottom variant="h5" fontWeight={700}>
-                  {news?.title.slice(0, 30)}...
-                </Typography>
-                <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                  By {news?.author} - {news?.date}
-                </Typography>
-                <Typography
-                  className="py-2"
-                  fontSize={17}
-                  sx={{ color: "text.secondary" }}
-                >
-                  {news?.description.slice(0, 100)}...
-                </Typography>
-              </CardContent>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  sx={{
+                    width: "100%",
+                    height: 250,
+                    objectFit: "cover",
+                  }}
+                  image={news?.image}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <span className="bg-red-500 px-4 py-1 text-white rounded-sm">
+                    {news?.category}
+                  </span>
+                  <Typography gutterBottom variant="h5" fontWeight={700}>
+                    {news?.title.slice(0, 30)}...
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                    By{" "}
+                    <span className="italic font-semibold">{news?.author}</span>{" "}
+                    - {news?.date}
+                  </Typography>
+                  <Typography
+                    className="py-2"
+                    fontSize={17}
+                    sx={{ color: "text.secondary" }}
+                  >
+                    {news?.description.slice(0, 100)}...
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
             </Card>
           </Grid>
         ))}
