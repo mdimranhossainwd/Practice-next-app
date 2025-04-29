29d4695e0f6a2a34d8c7d38e29ae4c1b6fce4f69
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-04-29 09:23:06 +0600

    refactor(card): added to category tag sections

--- src/app/catogories/[categoriesId]/page.tsx
+++ src/app/catogories/[categoriesId]/page.tsx
@@ -6,11 +6,6 @@ export default async function DynamicNews({ params, searchParams }) {
   const { data: allNewsData } = await getAllNews(searchParams?.category);
   return (
     <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
-      {/* <Container> automatically handles the left-right padding nicely */}
-      <h2 className="text-black mb-4">
-        Here's <b>All News</b> ({allNewsData?.length}) {searchParams?.category}
-      </h2>
-
       <Grid container spacing={2}>
         {allNewsData?.map((news) => (
           <Grid item xs={12} sm={6} md={4} key={news._id}>
@@ -22,6 +17,9 @@ export default async function DynamicNews({ params, searchParams }) {
                 alt="Banner Logo"
               />
               <CardContent>
+                <span className="bg-red-500 w-[250px] px-4 py-1 mb-3 text-white rounded-sm">
+                  {news?.category}
+                </span>
                 <Typography gutterBottom variant="h5" fontWeight={700}>
                   {news?.title.slice(0, 30)}...
                 </Typography>
